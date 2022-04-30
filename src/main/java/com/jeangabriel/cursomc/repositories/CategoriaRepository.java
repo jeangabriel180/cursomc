d6ed7667847c0f4d433ef0a7ee5deb0ec2495a01
Jean Gabriel <jeangabriel180@hotmail.com>
2022-04-29 22:51:00 -0300

    Criando repository e service para Categoria

---
+++ src/main/java/com/jeangabriel/cursomc/repositories/CategoriaRepository.java
@@ -0,0 +1,9 @@
+package com.jeangabriel.cursomc.repositories;
+
+import com.jeangabriel.cursomc.domain.Categoria;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface CategoriaRepository extends JpaRepository<Categoria, Integer> {
+}
--- src/main/java/com/jeangabriel/cursomc/resources/CategoriaResource.java
+++ src/main/java/com/jeangabriel/cursomc/resources/CategoriaResource.java
@@ -1,7 +1,11 @@
 package com.jeangabriel.cursomc.resources;
 
 import com.jeangabriel.cursomc.domain.Categoria;
+import com.jeangabriel.cursomc.services.CategoriaService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -12,13 +16,11 @@ import java.util.List;
 @RequestMapping(value = "/categorias")
 public class CategoriaResource {
 
-    @GetMapping
-    public List<Categoria> listar() {
-        Categoria cat1 = new Categoria(1, "Informática");
-        Categoria cat2 = new Categoria(2, "Escritório");
-        List<Categoria> lista = new ArrayList<>();
-        lista.add(cat1);
-        lista.add(cat2);
-        return lista;
+    @Autowired
+    CategoriaService service;
+
+    @GetMapping(value = "/{id}")
+    public ResponseEntity<?> find(@PathVariable Integer id) {
+        return ResponseEntity.ok(service.buscar(id));
     }
 }
