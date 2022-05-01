47b9239169d740f077472f937f60ecc0ed45fb46
Jean Gabriel <jeangabriel180@hotmail.com>
2022-05-01 20:57:15 -0300

    Estado e cidade

---
+++ src/main/java/com/jeangabriel/cursomc/repositories/CidadeRepository.java
@@ -0,0 +1,9 @@
+package com.jeangabriel.cursomc.repositories;
+
+import com.jeangabriel.cursomc.domain.Cidade;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface CidadeRepository extends JpaRepository<Cidade, Integer> {
+}
